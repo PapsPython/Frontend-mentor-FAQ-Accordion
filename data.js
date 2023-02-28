e4ff0be40a55fc0330125c127cf8d7e427f11769
Pa Yaw <117020650+PapsPython@users.noreply.github.com>
2023-02-28 19:12:24 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,20 @@
+export faqs
+
+const faqs = [
+  {
+    "question": "What is the maximum size of an upload?" ,
+   "answer": "A long sentence ",
+   "id" = 1
+  },
+   {
+    "question": "What is the maximum size of an upload?" ,
+   "answer": "A long sentence ",
+   "id" = 2
+  },
+  
+   {
+   "question": "What is the maximum size of an upload?" ,
+   "answer": "A long sentence ",
+   "id" = 3
+  }
+]
---
+++ script.js
@@ -0,0 +1,2 @@
+import {faqs} from 'data.js'
+
